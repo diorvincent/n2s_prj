dafdee36b70930a137cda9474574c0cd00dd8c7f
diorvincent <49552555+diorvincent@users.noreply.github.com>
2025-07-04 09:44:32 +0800

    RTE_Components

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,31 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GPIO' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "BF7006BMxx.h"
+
+#define BF7x06BMxx_ADC_DRIVER_PRESENT
+#define BF7x06BMxx_CAN_APP_DRIVER_PRESENT
+#define BF7x06BMxx_CAN_DRIVER_PRESENT
+#define BF7x06BMxx_EFLASH_DRIVER_PRESENT
+#define BF7x06BMxx_GPIO_DRIVER_PRESENT
+#define BF7x06BMxx_LIN_DRIVER_PRESENT
+#define BF7x06BMxx_PWM_DRIVER_PRESENT
+#define BF7x06BMxx_RTC_DRIVER_PRESENT
+#define BF7x06BMxx_SCI_DRIVER_PRESENT
+#define BF7x06BMxx_TIMER_DRIVER_PRESENT
+#define BF7x06BMxx_WDT_DRIVER_PRESENT
+
+#endif /* RTE_COMPONENTS_H */
